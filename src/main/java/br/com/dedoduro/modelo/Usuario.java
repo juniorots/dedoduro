6db0e1b3caace64ae0982c1a1ff885ef4888f377
juniorots <juniorots@gmail.com>
2015-06-28 19:41:36 -0300

    Atualizando...

--- src/main/java/br/com/dedoduro/modelo/Usuario.java
+++ src/main/java/br/com/dedoduro/modelo/Usuario.java
@@ -6,30 +6,33 @@
 
 package br.com.dedoduro.modelo;
 
+import br.com.dedoduro.framework.persistence.DomainObject;
 import java.util.LinkedList;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  *
  * @author Jose Alves
  */
 @Entity
-public class Usuario {
+public class Usuario extends DomainObject {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long codigoUsuario;
     
-    @Column( nullable = false )
+    @NotNull
     private String email;
+    
+    @NotNull
     private String senha;
     
+    @NotNull
     private String receberEmail; // S - Sim; N - Nao;
     
     @ManyToMany
