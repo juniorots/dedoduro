ce372eb114967e8aa93288daeeb9dfab6406e9f8
juniorots <juniorots@gmail.com>
2015-07-09 16:31:49 -0300

    Trabalhando na janela de login

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -70,3 +70,26 @@ $("#cssmenu").menumaker({
 
 });
 })(jQuery);
+
+/*
+ * Customizacao para janela de login
+ */
+$(function() {
+    var button = $('#loginButton');
+    var box = $('#loginBox');
+    var form = $('#loginForm');
+    button.removeAttr('href');
+    button.mouseup(function(login) {
+        box.toggle();
+        button.toggleClass('active');
+    });
+    form.mouseup(function() { 
+        return false;
+    });
+    $(this).mouseup(function(login) {
+        if(!($(login.target).parent('#loginButton').length > 0)) {
+            button.removeClass('active');
+            box.hide();
+        }
+    });
+});
