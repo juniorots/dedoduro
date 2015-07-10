bbe0151ace6a9ee484a1dfbe6947bc218a4af84d
juniorots <juniorots@gmail.com>
2015-07-10 17:52:00 -0300

    Atualizando menu

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -42,19 +42,23 @@
         if (settings.format === 'multitoggle') multiTg();
         else cssmenu.addClass('dropdown');
 
-//        if (settings.sticky === true) cssmenu.css('position', 'fixed');
+        if (settings.sticky === true) cssmenu.css('position', 'fixed');
 
-//        resizeFix = function() {
-//          if ($( window ).width() > 768) {
+        resizeFix = function() {
+            
+            // TRABALHANDO ... :-)
+          $('liCustomizado').css('padding-left', '100px');    
+
+            if ($( window ).width() > 768) {
 //            cssmenu.find('ul').show();
-//          }
-//
-//          if ($(window).width() <= 768) {
+          }
+
+          if ($(window).width() <= 768) {
 //            cssmenu.find('ul').hide().removeClass('open');
-//          }
-//        };
-//        resizeFix();
-//        return $(window).on('resize', resizeFix);
+          }
+        };
+        resizeFix();
+        return $(window).on('resize', resizeFix);
 
       });
   };
