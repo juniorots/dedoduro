a7d8c58f2d741c73211ccd8938679f7daab9d817
juniorots <juniorots@gmail.com>
2015-07-31 16:23:09 -0300

    Atualizando

--- src/main/webapp/resources/js/principal.js
+++ src/main/webapp/resources/js/principal.js
@@ -5,11 +5,14 @@ var bancas = [
 
 function fecharOutrasCestas( banca ) {
     var closespeed = 300;
+    
     for (i = 0; i < bancas.length; i++) {
-        if (bancas[i] !== banca) {
+        if ( (bancas[i] !== banca) && ( $(bancas[i]).find('ul').attr('class') == 'openStack' ) ) {
+            $(bancas[i]).find('ul').removeClass('openStack').children('li').animate({top: '72px', left: '-10px'}, closespeed);
+            $("img", $(bancas[i]) ).animate({width: '72px'}, 100);
+//            $(bancas[i]).find('ul').animate({top: '465px', left: '190px'}, closespeed);
 //            $(bancas[i]).removeClass('openStack').children('li').animate({top: '72px', left: '-110px'}, closespeed);
-            $(bancas[i]).find('ul').animate({top: '478px', left: '52px'}, closespeed);
-            $(bancas[i]).find('li a>img').animate({width: '71px', marginLeft: '99'}, closespeed);
+//            $(bancas[i]).find('li a>img').animate({width: '71px', marginLeft: '99'}, closespeed);
         }
     }
 }
