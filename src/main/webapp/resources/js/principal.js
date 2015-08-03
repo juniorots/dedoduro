1e95e0633f9778204959dc4478da3c1505d3ad38
juniorots <juniorots@gmail.com>
2015-08-03 15:21:46 -0300

    novo item de submenu

--- src/main/webapp/resources/js/principal.js
+++ src/main/webapp/resources/js/principal.js
@@ -15,9 +15,11 @@ function fecharOutrasCestas( banca ) {
         if ( (bancas[i] !== banca) && ( $(bancas[i]).find('ul').attr('class') == 'openStack' ) ) {
             $(bancas[i]).find('ul').removeClass('openStack').children('li').animate({top: '72px', left: '-10px'}, closespeed);
             
-            if ( $(bancas[i]) === '.stackFcc') {
+            if ( bancas[i] === '.stackFcc') {
+                $(bancas[i]).find('ul').removeClass('openStack').children('li').animate({top: '72px', left: '-5px'}, closespeed);
                 $(bancas[i]).find('ul > li > a > img').animate({width: '65px'}, 100);
             } else {
+                // default case
                 $(bancas[i]).find('ul > li > a > img').animate({width: '71px'}, 100);
             }
             $(bancas[i]).find('ul > li > a > span').animate({marginRight: '0px'});
