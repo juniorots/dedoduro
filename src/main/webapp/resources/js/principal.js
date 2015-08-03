43476b6b79329103200572bb98567fde0c093f59
juniorots <juniorots@gmail.com>
2015-08-03 00:44:38 -0300

    Ajustando posicionamento

--- src/main/webapp/resources/js/principal.js
+++ src/main/webapp/resources/js/principal.js
@@ -13,7 +13,7 @@ function fecharOutrasCestas( banca ) {
     
     for (i = 0; i < bancas.length; i++) {
         if ( (bancas[i] !== banca) && ( $(bancas[i]).find('ul').attr('class') == 'openStack' ) ) {
-            $(bancas[i]).find('ul').removeClass('openStack').children('li').animate({top: '72px', left: '-7px'}, closespeed);
+            $(bancas[i]).find('ul').removeClass('openStack').children('li').animate({top: '72px', left: '-10px'}, closespeed);
             $(bancas[i]).find('ul > li > a > img').animate({width: '71px'}, 100);
             $(bancas[i]).find('ul > li > a > span').animate({marginRight: '0px'});
         }
--- src/main/webapp/resources/js/scriptQuadrix.js
+++ src/main/webapp/resources/js/scriptQuadrix.js
@@ -23,31 +23,10 @@ $(function () {
         } else {
             //reverse above
             var $el=$(this);
-            $el.next().removeClass('openStack').children('li').animate({top: '72px', left: '-109px'}, closespeed);
+            $el.next().removeClass('openStack').children('li').animate({top: '72px', left: '-112px'}, closespeed);
             $el.next().find('li a>img').animate({width: '71px', marginLeft: '102'}, closespeed);
         }
     });
-    
-    // DEPRECATED !! 
-//    $('.stackQuadrix>img').toggle(function(){
-//        var vertical = 0;
-//        var horizontal = 0;
-//        var $el=$(this);
-//        $el.next().children().each(function(){
-//            $(this).animate({top: '-' + vertical + 'px', left: horizontal + 'px'}, openspeed);
-//            vertical = vertical + 55;
-//            horizontal = (horizontal+.75)*2;
-//        });
-//        $el.next().animate({top: '405px', left: '348px'}, openspeed).addClass('openStack')
-//            .find('li a>img').animate({width: '60px', marginLeft: '0px'}, openspeed);
-//        $el.animate({paddingTop: '0'});
-//        fecharOutrasCestas('.stackQuadrix');
-//    }, function(){
-//        //reverse above
-//        var $el=$(this);
-//        $el.next().removeClass('openStack').children('li').animate({top: '70px', left: '-109px'}, closespeed);
-//        $el.next().find('li a>img').animate({width: '71px', marginLeft: '102'}, closespeed);
-//    });
      
     // Stacks additional animation
     $('.stackQuadrix li a').hover(function(){
