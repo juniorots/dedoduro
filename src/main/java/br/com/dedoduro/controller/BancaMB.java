4f3eaf92556d6de93760bb04bcf92c8a2493302d
juniorots <juniorots@gmail.com>
2015-08-11 14:39:37 -0300

    Listagem geral

--- src/main/java/br/com/dedoduro/controller/BancaMB.java
+++ src/main/java/br/com/dedoduro/controller/BancaMB.java
@@ -9,8 +9,12 @@ package br.com.dedoduro.controller;
 import br.com.dedoduro.modelo.Banca;
 import java.util.ArrayList;
 import java.util.Collection;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.TabChangeEvent;
+import org.primefaces.event.TabCloseEvent;
 
 /**
  *
@@ -46,6 +50,16 @@ public class BancaMB {
 
     public void setListaBanca(Collection<Banca> listaBanca) {
         this.listaBanca = listaBanca;
+    }
+    
+    public void onTabChange(TabChangeEvent event) {
+        FacesMessage msg = new FacesMessage("Ativado", event.getTab().getTitle());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+         
+    public void onTabClose(TabCloseEvent event) {
+        FacesMessage msg = new FacesMessage("Desativado", event.getTab().getTitle());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
 }
